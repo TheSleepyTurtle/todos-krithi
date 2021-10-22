4fa47e743a250caa07146e36a005b84e0e0a903f
KrithiGanapathy <79409282+KrithiGanapathy@users.noreply.github.com>
2021-10-22 12:59:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from tinydb import TinyDB, Query
+from flask import Flask, render_template, redirect
+
+db = TinyDB('db.json')
+Todo = Query()
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html', todos=db.all())
+
+
+@app.route('/add/<todo>/<author>')
+def add_todo(author, todo):
+	db.insert({
+		'todo': todo,
+		'author': author
+	})
+	return redirect('/')
+
+
+@app.route('/add')
+def add():
+	return render_template('add.html')
+
+
+@app.route('/delete/<name>')
+def delete(name):
+	db.remove(Todo.todo == name)
+	return redirect('/')
+
+if __name__ == '__main__':
+	app.run(debug=True)
